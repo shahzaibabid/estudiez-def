aee8e6a07842dbc98e783085a904ad7c1bfed58b
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-08-11 15:40:47 +0500

    header & footer

---
+++ header.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudiez-Header</title>
+<!-- Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/animate.min.css" rel="stylesheet" type="text/css">
+<link href="css/javascript-plugins-bundle.css" rel="stylesheet"/>
+<!-- CSS | menuzord megamenu skins -->
+<link href="js/menuzord/css/menuzord.css" rel="stylesheet"/>
+<!-- CSS | Main style file -->
+<link href="css/style-main.css" rel="stylesheet" type="text/css">
+<link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-rounded-boxed.css" rel="stylesheet"/>
+
+<!-- CSS | Responsive media queries -->
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+
+<!-- CSS | Theme Color -->
+<link href="css/colors/theme-skin-color-set1.css" rel="stylesheet" type="text/css">
+
+<!-- external javascripts -->
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/javascript-plugins-bundle.js"></script>
+<script src="js/menuzord/js/menuzord.js"></script>
+
+<!-- REVOLUTION STYLE SHEETS -->
+<link rel="stylesheet" type="text/css" href="js/revolution-slider/css/rs6.css">
+<link rel="stylesheet" type="text/css" href="js/revolution-slider/extra-rev-slider1.css">
+<!-- REVOLUTION LAYERS STYLES -->
+<!-- REVOLUTION JS FILES -->
+<script src="js/revolution-slider/js/revolution.tools.min.js"></script>
+<script src="js/revolution-slider/js/rs6.min.js"></script>
+<script src="js/revolution-slider/extra-rev-slider1.js"></script>
+
+</head>
+<body>
+    <!-- header start -->
+<header id="header" class="header header-layout-type-header-2rows">
+    <div class="header-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-auto header-top-left align-self-center text-center text-xl-start">
+            <ul class="element contact-info">
+              <li class="contact-phone"><i class="fa fa-phone font-icon sm-display-block"></i> Tel: +440-98-5298</li>
+              <li class="contact-email"><i class="fa fa-envelope font-icon sm-display-block"></i> info@example.com</li>
+              <li class="contact-address"><i class="fa fa-map font-icon sm-display-block"></i> 121 King Street, Melbourne</li>
+            </ul>
+          </div>
+          <div class="col-xl-auto ms-xl-auto header-top-right align-self-center text-center text-xl-end">
+            <div class="element pt-0 pb-0">
+              <ul class="styled-icons icon-dark icon-theme-colored1 icon-circled clearfix">
+                <li><a class="social-link" href="#" ><i class="fab fa-facebook"></i></a></li>
+                <li><a class="social-link" href="#" ><i class="fab fa-twitter"></i></a></li>
+                <li><a class="social-link" href="#" ><i class="fab fa-youtube"></i></a></li>
+              </ul>
+            </div>
+            <div class="element pt-0 pt-lg-10 pb-0">
+              <a href="ajax-load/form-appointment.html" class="btn btn-theme-colored2 btn-sm ajaxload-popup">Make an Appointment</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-nav tm-enable-navbar-hide-on-scroll">
+      <div class="header-nav-wrapper navbar-scrolltofixed">
+        <div class="menuzord-container header-nav-container">
+          <div class="container position-relative">
+            <div class="row header-nav-col-row">
+              <div class="col-sm-auto align-self-center">
+                <a class="menuzord-brand site-brand" href="index-mp-layout1.html">
+                  <img class="logo-default logo-1x" src="images/logo-wide.png" alt="Logo">
+                  <img class="logo-default logo-2x retina" src="images/logo-wide%402x.png" alt="Logo">
+                </a>
+              </div>
+              <div class="col-sm-auto ms-auto pr-0 align-self-center">
+                <nav id="top-primary-nav" class="menuzord theme-color1" data-effect="fade" data-animation="none" data-align="right">
+                  <ul id="main-nav" class="menuzord-menu onepage-nav">
+                    <li class="active"><a href="#home">Home</a> </li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#courses">Courses</a></li>
+                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#team">Teachers</a></li>
+                    <li><a href="#news">Blog</a></li>
+                  </ul>
+                </nav>
+              </div>
+              <div class="col-sm-auto align-self-center nav-side-icon-parent">
+                <ul class="list-inline nav-side-icon-list">
+                  <li class="hidden-mobile-mode"><a href="#" id="top-nav-search-btn"><i class="search-icon fa fa-search"></i></a></li>
+                  <li class="hidden-mobile-mode">
+                    <div class="top-nav-mini-cart-icon-container">
+                      <div class="top-nav-mini-cart-icon-contents">
+                        <a class="mini-cart-icon" href="shop-cart.html" title="View your shopping cart">
+                          <img src="images/shopping-cart.png" width="25" alt="cart"><span class="items-count">1</span> <span class="cart-quick-info">1 item - <span class="amount"><span class="currencySymbol">&pound;</span>18.00</span></span>
+                        </a>
+                        <div class="dropdown-content">
+                          <ul class="cart_list product_list_widget">
+                            <li class="mini_cart_item">
+                              <a href="#" class="remove remove_from_cart_button" aria-label="Remove this item" data-product_id="18832" data-cart_item_key="#" data-product_sku="woo-beanie">&times;</a>
+                              <a href="#"> <img class="attachment-thumbnail" src="images/shop/product.jpg" width="300" height="300" alt=""/>Beanie</a>
+                              <span class="quantity">1 &times; <span class="amount">
+                              <span class="currencySymbol">&pound;</span>18.00</span></span>
+                            </li>
+                          </ul>
+                          <p class="total"> <strong>Subtotal:</strong> <span class="woocommerce-Price-amount amount"><span class="currencySymbol">&pound;</span>18.00</span> </p>
+                          <div class="buttons cart-action-buttons">
+                            <div class="row">
+                              <div class="col-6 pe-0"><a href="shop-cart.html" class="btn btn-theme-colored2 btn-block btn-sm wc-forward">View Cart</a></div>
+                              <div class="col-6 ps-1"><a href="shop-checkout.html" class="btn btn-theme-colored1 btn-block btn-sm checkout wc-forward">Checkout</a></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="hidden-mobile-mode">
+                    <div id="side-panel-trigger" class="side-panel-trigger">
+                      <a href="#">
+                        <div class="hamburger-box">
+                          <div class="hamburger-inner"></div>
+                        </div>
+                        </a>
+                    </div>
+                  </li>
+                </ul>
+                <div id="top-nav-search-form" class="clearfix">
+                  <form action="#" method="GET">
+                    <input type="text" name="s" value="" placeholder="Type and Press Enter..." autocomplete="off" />
+                  </form>
+                  <a href="#" id="close-search-btn"><i class="fa fa-times"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="row d-block d-xl-none">
+               <div class="col-12">
+                <nav id="top-primary-nav-clone" class="menuzord d-block d-xl-none default menuzord-color-default menuzord-border-boxed menuzord-responsive" data-effect="slide" data-animation="none" data-align="right">
+                 <ul id="main-nav-clone" class="menuzord-menu menuzord-right menuzord-indented scrollable">
+                 </ul>
+                </nav>
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header> 
+  <!-- header end -->
+</body>
+</html>
