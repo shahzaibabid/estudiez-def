b5602e95f21b3ae932a1156af18d62965de840fc
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-08-12 17:51:57 +0500

    header updated

--- header.php
+++ header.php
@@ -1,171 +1,84 @@
-<div class="side-panel-body-overlay"></div>
-<div id="side-panel-container" class="dark" data-tm-bg-img="images/side-push-bg.jpg">
-  <div class="side-panel-wrap">
-    <div id="side-panel-trigger-close" class="side-panel-trigger"><a href="#"><i class="fa fa-times side-panel-trigger-icon"></i></a></div>
-    <img class="logo mb-50" src="images/logo-wide.png" alt="Logo">
-    <p>Lorem ipsum is simply free text dolor sit am adipi we help you ensure everyone is in the right jobs sicing elit, sed do consulting firms Et leggings across the nation tempor.</p>
-    <div class="widget">
-      <h4 class="widget-title widget-title-line-bottom line-bottom-theme-colored1">Latest News</h4>
-      <div class="latest-posts">
-        <article class="post clearfix pb-0 mb-10">
-          <a class="post-thumb" href="news-details.html"><img src="images/blog/s1.jpg" alt="images"></a>
-          <div class="post-right">
-            <h5 class="post-title mt--0"><a href="news-details.html">Sustainable Construction</a></h5>
-            <p>Lorem ipsum dolor...</p>
-          </div>
-        </article>
-        <article class="post clearfix pb-0 mb-10">
-          <a class="post-thumb" href="news-details.html"><img src="images/blog/s2.jpg" alt="images"></a>
-          <div class="post-right">
-            <h5 class="post-title mt--0"><a href="news-details.html">Industrial Coatings</a></h5>
-            <p>Lorem ipsum dolor...</p>
-          </div>
-        </article>
-        <article class="post clearfix pb-0 mb-10">
-          <a class="post-thumb" href="news-details.html"><img src="images/blog/s3.jpg" alt="images"></a>
-          <div class="post-right">
-            <h5 class="post-title mt--0"><a href="news-details.html">Storefront Installations</a></h5>
-            <p>Lorem ipsum dolor...</p>
-          </div>
-        </article>
-      </div>
-    </div>
 
-    <div class="widget">
-      <h5 class="widget-title widget-title-line-bottom line-bottom-theme-colored1">Contact Info</h5>
-      <div class="tm-widget-contact-info contact-info-style1 contact-icon-theme-colored1">
-        <ul>
-          <li class="contact-name">
-            <div class="icon"><i class="flaticon-contact-037-address"></i></div>
-            <div class="text">John Doe</div>
-          </li>
-          <li class="contact-phone">
-            <div class="icon"><i class="flaticon-contact-042-phone-1"></i></div>
-            <div class="text"><a href="tel:+510-455-6735">+510-455-6735</a></div>
-          </li>
-          <li class="contact-email">
-            <div class="icon"><i class="flaticon-contact-043-email-1"></i></div>
-            <div class="text"><a href="mailto:info@yourdomain.com">info@yourdomain.com</a></div>
-          </li>
-          <li class="contact-address">
-            <div class="icon"><i class="flaticon-contact-047-location"></i></div>
-            <div class="text">3982 Browning Lane Carolyns Circle, California</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div> 
 <div id="wrapper" class="clearfix">
   <!-- Header -->
     <header id="header" class="header header-layout-type-header-2rows">
-    <div class="header-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-xl-auto header-top-left align-self-center text-center text-xl-start">
-            <ul class="element contact-info">
-              <li class="contact-phone"><i class="fa fa-phone font-icon sm-display-block"></i> Tel: +440-98-5298</li>
-              <li class="contact-email"><i class="fa fa-envelope font-icon sm-display-block"></i> info@example.com</li>
-              <li class="contact-address"><i class="fa fa-map font-icon sm-display-block"></i> 121 King Street, Melbourne</li>
-            </ul>
-          </div>
-          <div class="col-xl-auto ms-xl-auto header-top-right align-self-center text-center text-xl-end">
-            <div class="element pt-0 pb-0">
-              <ul class="styled-icons icon-dark icon-theme-colored1 icon-circled clearfix">
-                <li><a class="social-link" href="#" ><i class="fab fa-facebook"></i></a></li>
-                <li><a class="social-link" href="#" ><i class="fab fa-twitter"></i></a></li>
-                <li><a class="social-link" href="#" ><i class="fab fa-youtube"></i></a></li>
-              </ul>
-            </div>
-            <div class="element pt-0 pt-lg-10 pb-0">
-              <a href="ajax-load/form-appointment.html" class="btn btn-theme-colored2 btn-sm ajaxload-popup">Make an Appointment</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="header-nav tm-enable-navbar-hide-on-scroll">
-      <div class="header-nav-wrapper navbar-scrolltofixed">
-        <div class="menuzord-container header-nav-container">
-          <div class="container position-relative">
-            <div class="row header-nav-col-row">
-              <div class="col-sm-auto align-self-center">
-                <a class="menuzord-brand site-brand" href="index-mp-layout1.html">
-                  <img class="logo-default logo-1x" src="images/logo-wide.png" alt="Logo">
-                  <img class="logo-default logo-2x retina" src="images/logo-wide%402x.png" alt="Logo">
-                </a>
-              </div>
-              <div class="col-sm-auto ms-auto pr-0 align-self-center">
-                <nav id="top-primary-nav" class="menuzord theme-color1" data-effect="fade" data-animation="none" data-align="right">
-                  <ul id="main-nav" class="menuzord-menu onepage-nav">
-                    <li class="active"><a href="index.php">Home</a> </li>
-                    <li><a href="aboutus.php">About-Us</a></li>
-                    <li><a href="contactus.php">Contact-Us</a></li>
-                    <li><a href="feedback.php">Feedback</a></li>
-                    <li><a href="register.php">Register</a></li>
-                    <li><a href="login.php">Login</a></li>
-                  </ul>
-                </nav>
-              </div>
-              <div class="col-sm-auto align-self-center nav-side-icon-parent">
-                <ul class="list-inline nav-side-icon-list">
-                  <li class="hidden-mobile-mode"><a href="#" id="top-nav-search-btn"><i class="search-icon fa fa-search"></i></a></li>
-                  <li class="hidden-mobile-mode">
-                    <div class="top-nav-mini-cart-icon-container">
-                      <div class="top-nav-mini-cart-icon-contents">
-                        <a class="mini-cart-icon" href="shop-cart.html" title="View your shopping cart">
-                          <img src="images/shopping-cart.png" width="25" alt="cart"><span class="items-count">1</span> <span class="cart-quick-info">1 item - <span class="amount"><span class="currencySymbol">&pound;</span>18.00</span></span>
-                        </a>
-                        <div class="dropdown-content">
-                          <ul class="cart_list product_list_widget">
-                            <li class="mini_cart_item">
-                              <a href="#" class="remove remove_from_cart_button" aria-label="Remove this item" data-product_id="18832" data-cart_item_key="#" data-product_sku="woo-beanie">&times;</a>
-                              <a href="#"> <img class="attachment-thumbnail" src="images/shop/product.jpg" width="300" height="300" alt=""/>Beanie</a>
-                              <span class="quantity">1 &times; <span class="amount">
-                              <span class="currencySymbol">&pound;</span>18.00</span></span>
-                            </li>
-                          </ul>
-                          <p class="total"> <strong>Subtotal:</strong> <span class="woocommerce-Price-amount amount"><span class="currencySymbol">&pound;</span>18.00</span> </p>
-                          <div class="buttons cart-action-buttons">
-                            <div class="row">
-                              <div class="col-6 pe-0"><a href="shop-cart.html" class="btn btn-theme-colored2 btn-block btn-sm wc-forward">View Cart</a></div>
-                              <div class="col-6 ps-1"><a href="shop-checkout.html" class="btn btn-theme-colored1 btn-block btn-sm checkout wc-forward">Checkout</a></div>
+
+      <div class="header-nav tm-enable-navbar-hide-on-scroll">
+        <div class="header-nav-wrapper navbar-scrolltofixed">
+          <div class="menuzord-container header-nav-container">
+            <div class="container position-relative">
+              <div class="row header-nav-col-row">
+                <div class="col-sm-auto align-self-center">
+                  <a class="menuzord-brand site-brand" href="index-mp-layout1.html">
+                    <img class="logo-default logo-1x" src="images/logo-wide.png" alt="Logo">
+                    <img class="logo-default logo-2x retina" src="images/logo-wide%402x.png" alt="Logo">
+                  </a>
+                </div>
+                <div class="col-sm-auto ms-auto pr-0 align-self-center">
+                  <nav id="top-primary-nav" class="menuzord theme-color1" data-effect="fade" data-animation="none" data-align="right">
+                    <ul id="main-nav" class="menuzord-menu onepage-nav">
+                      <li class="active"><a href="index.php">Home</a> </li>
+                      <li><a href="aboutus.php">About-Us</a></li>
+                      <li><a href="contactus.php">Contact-Us</a></li>
+                      <li><a href="feedback.php">Feedback</a></li>
+                      <li><a href="register.php">Register</a></li>
+                      <li><a href="login.php">Login</a></li>
+                    </ul>
+                  </nav>
+                </div>
+                <div class="col-sm-auto align-self-center nav-side-icon-parent">
+                  <ul class="list-inline nav-side-icon-list">
+                 
+                    <li class="hidden-mobile-mode">
+                      <div class="top-nav-mini-cart-icon-container">
+                        <div class="top-nav-mini-cart-icon-contents">
+                          <a class="mini-cart-icon"  title="View your shopping cart">
+                            <img src="https://i.pinimg.com/474x/91/97/db/9197dbd033174b03af42d396f11dc49e.jpg" width="25" alt="profile"> <span class="cart-quick-info">1 item - <span class="amount"><span class="currencySymbol">&pound;</span>18.00</span></span>
+                          </a>
+                          <div class="dropdown-content">
+                            <ul class="cart_list product_list_widget">
+                            
+                              
+                              <center>
+                               <img class="attachment-thumbnail" src="images/shop/product.jpg" style="height: 80px; border-radius: 100%;" alt=""/>
+                               <h4 class="total"> <strong>name</strong> <span class="woocommerce-Price-amount amount"><span class="currencySymbol"> </h3>
+                              
+                              </center>
+                         
+                             
+                            </ul>
+                            
+                            <div class="buttons cart-action-buttons">
+                              <div class="row">
+                                <div class="col-6 pe-0"><a href="" class="btn btn-theme-colored2 btn-block btn-sm wc-forward">Profile</a></div>
+                                <div class="col-6 ps-1"><a href="" class="btn btn-theme-colored1 btn-block btn-sm checkout wc-forward">Logout</a></div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                  <li class="hidden-mobile-mode">
-                    <div id="side-panel-trigger" class="side-panel-trigger">
-                      <a href="#">
-                        <div class="hamburger-box">
-                          <div class="hamburger-inner"></div>
-                        </div>
-                        </a>
-                    </div>
-                  </li>
-                </ul>
-                <div id="top-nav-search-form" class="clearfix">
-                  <form action="#" method="GET">
-                    <input type="text" name="s" value="" placeholder="Type and Press Enter..." autocomplete="off" />
-                  </form>
-                  <a href="#" id="close-search-btn"><i class="fa fa-times"></i></a>
+                    </li>
+                  </ul>
+                  <div id="top-nav-search-form" class="clearfix">
+                    <form action="#" method="GET">
+                      <input type="text" name="s" value="" placeholder="Type and Press Enter..." autocomplete="off" />
+                    </form>
+                    <a href="#" id="close-search-btn"><i class="fa fa-times"></i></a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="row d-block d-xl-none">
-               <div class="col-12">
-                <nav id="top-primary-nav-clone" class="menuzord d-block d-xl-none default menuzord-color-default menuzord-border-boxed menuzord-responsive" data-effect="slide" data-animation="none" data-align="right">
-                 <ul id="main-nav-clone" class="menuzord-menu menuzord-right menuzord-indented scrollable">
-                 </ul>
-                </nav>
-               </div>
+              <div class="row d-block d-xl-none">
+                <div class="col-12">
+                  <nav id="top-primary-nav-clone" class="menuzord d-block d-xl-none default menuzord-color-default menuzord-border-boxed menuzord-responsive" data-effect="slide" data-animation="none" data-align="right">
+                  <ul id="main-nav-clone" class="menuzord-menu menuzord-right menuzord-indented scrollable">
+                  </ul>
+                  </nav>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>  
+    </header>  
   <!-- Start main-content -->
   <div class="main-content-area">
