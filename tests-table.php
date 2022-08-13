2b33792057a498510e3ed22daa2e3b557cd0d2a6
Zozo-pk <zohairjafar1190@gmail.com>
2022-08-13 10:47:01 +0500

    student profile page created
    updated style-main.css and tm-utility-classes.css to add some css properties.
    
    added profile.php
    added files for tables used in profile.php

---
+++ tests-table.php
@@ -0,0 +1,104 @@
+
+	<div class="tm-sc-section-title section-title text-center">
+                    <div class="title-wrapper">
+                    <h2 class="title">Tests <span class="text-theme-colored3">Table</span></h2>
+                    </div>
+    </div>
+	<table border="5" cellspacing="0" align="center">
+		<!--<caption>Timetable</caption>-->
+		<tr>
+			<td align="center" height="50"
+				width="100">
+				<b>ID</b>
+			</td>
+			<td align="center" height="50"
+				width="100">
+				<b>Date</b>
+			</td>
+			<td align="center" height="50"
+				width="100">
+				<b>Class ID</b>
+			</td>
+			<td align="center" height="50"
+				width="100">
+				<b>User ID</b>
+			</td>
+			<td align="center" height="50"
+				width="100">
+				<b>Total Marks</b>
+			</td>
+			<td align="center" height="50"
+				width="100">
+				<b>Obtained Marks</b>
+			</td>
+			<td align="center" height="50"
+				width="100">
+				<b>Status</b>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" height="50">
+				<b>Monday</b></td>
+			<td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+		</tr>
+		<tr>
+			<td align="center" height="50">
+				<b>Tuesday</b>
+			</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+		</tr>
+		<tr>
+			<td align="center" height="50">
+				<b>Wednesday</b>
+			</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+		</tr>
+		<tr>
+			<td align="center" height="50">
+				<b>Thursday</b>
+			</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+		</tr>
+		<tr>
+			<td align="center" height="50">
+				<b>Friday</b>
+			</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+		</tr>
+		<tr>
+			<td align="center" height="50">
+				<b>Saturday</b>
+			</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+			<td align="center" height="50">Mat</td>
+			<td align="center" height="50">Che</td>
+            <td align="center" height="50">Eng</td>
+		</tr>
+	</table>
