fc1d241dee0773515a8d558a8f3e8ac2e17498c9
Zozo-pk <zohairjafar1190@gmail.com>
2022-08-14 13:18:22 +0500

    updated links.php again
    made it more readable

--- links.php
+++ links.php
@@ -1,8 +1,24 @@
 <?php
-$target = "text.txt";
-$linkname = "mylink"; 
+// fail grade link + number here
+$target1 = "add numeric value here from db";
+$linkname1 = "https://giphy.com/gifs/next-time-S4BDGxHKIB6nW9PiyA?utm_source=media-link&utm_medium=landing&utm_campaign=Media%20Links&utm_term=https://giphy.com/"; 
 
-link($target,$linkname);
+// just passing grade link + number here
+$target2 = "add numeric value here from db";
+$linkname2 = "https://www.joyce.edu/blog/study-tips-to-retain-information/"; 
+
+// high grade link + number here
+$target3 = "add numeric value here from db";
+$linkname3 = "https://i.pinimg.com/originals/af/ff/52/afff522b4f568a7c897c8511b66dcb35.gif"; 
+
+// class topper link + number here
+$target4 = "add numeric value here from db";
+$linkname4 = "https://www.oxfordlearning.com/how-to-study-effectively/"; 
+
+link1($target1,$linkname1);
+link2($target2,$linkname2);
+link3($target3,$linkname3);
+link4($target4,$linkname4);
 ?>
 
 <div class="tm-sc-section-title section-title text-center">
